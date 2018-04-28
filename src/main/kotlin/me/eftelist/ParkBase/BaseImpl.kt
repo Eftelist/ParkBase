95cf4a220718ac0127d63bbfc8ba735179f1844e
VPS_h2780115 <buildtools@h2780115.com>
2018-04-28 12:51:58 +0200

    Added Essentials & instance variable

--- src/main/kotlin/me/eftelist/ParkBase/BaseImpl.kt
+++ src/main/kotlin/me/eftelist/ParkBase/BaseImpl.kt
@@ -1,5 +1,8 @@
 package me.eftelist.ParkBase
 
+import com.earth2me.essentials.Essentials
+import org.bukkit.Bukkit
+
 class BaseImpl : ParkBase() {
 
     override fun loadDependencies() {
@@ -11,11 +14,19 @@ class BaseImpl : ParkBase() {
     }
 
     override fun enable() {
+        instance = this;
+        essentials = Bukkit.getPluginManager().getPlugin("Essentials") as Essentials
         logger.info("Great. It works!")
     }
 
     override fun disable() {
         logger.info("Bye.")
+        instance = null;
+    }
+
+    companion object {
+        var instance: BaseImpl? = null
+        var essentials: Essentials? = null
     }
 
 }
