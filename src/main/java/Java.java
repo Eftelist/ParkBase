f9f8248f81add6abc354fbbe1931a7f98a47dc28
VPS_h2780115 <buildtools@h2780115.com>
2018-04-28 16:41:25 +0200

    Milan! Check dan hihi

--- src/main/java/Java.java
+++ src/main/java/Java.java
@@ -1,24 +1,39 @@
+import me.eftelist.ParkBase.Base.Dependency;
 import me.eftelist.ParkBase.Base.ParkBase;
+import me.eftelist.ParkBase.Commands.Builders.Action;
+import me.eftelist.ParkBase.Commands.Builders.CommandBuilder;
+import me.eftelist.ParkBase.Commands.Builders.SubCommandBuilder;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
 
 public class Java extends ParkBase {
-    
+
     @Override
     public void loadCommands() {
-
+        addCommand(new CommandBuilder().setCommand("hi").addSubCommand(
+                new SubCommandBuilder().name("world").action(
+                        new Action() {
+                            @Override
+                            public void execute(@NotNull Player p) {
+                                p.sendMessage("Hallo Wereld!");
+                            }
+                        }
+                ).build()
+        ).build());
     }
 
     @Override
     public void loadDependencies() {
-
+        addDependency(new Dependency("ProtocolLib"));
     }
 
     @Override
     public void disable() {
-
+        getLogger().info("Doei");
     }
 
     @Override
     public void enable() {
-
+        getLogger().info("Hoi");
     }
 }
